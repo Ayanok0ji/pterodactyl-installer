e0e1591fa710f43f39df02ee9224f215613a3623
Ayanokoji <104145422+Ayanok0ji@users.noreply.github.com>
2026-03-09 21:09:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v0.12.2"
 export SCRIPT_RELEASE="v0.12.2"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/Ayanok0ji/pterodactyl-installer"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
